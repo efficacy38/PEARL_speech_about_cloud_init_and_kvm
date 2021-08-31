9558b3b6ed08d96e779b1f61890eed04f9cbc46a
jerry <efficacy38@gmail.com>
2021-08-31 22:42:16 +0900

    update demo1's userdata

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PEARL_speech_about_cloud_init_and_kvm
+
+## utils
+### <span>boot.sh</span>
+can use this shell to boot a virtual machine with qemu, and need to input the generated image name while using the shell.
+
+### <span>genseed.sh</span>
+can use this shell to generate the seed file aka configure driver(in openstack)
+
+## demos
+which contain some examples, demostrate the cloud init's feature, and some application.]
+### files in demo
+- userdata.yaml
+    - as cloud-init's userdata
+- metadata.yaml
+    - as cloud-init's metadata
+    - as openstack's metadata service
--- kvm_utils/boot.sh
+++ kvm_utils/boot.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 IMG="focal-server-cloudimg-amd64.img"
+TARGET_IMG="fallback-cloudimg.img"
 SEED="seed.img"
 
-if [ ! -e "$IMG" ]; then
-	echo "Img not exist."
-    echo "run $ 'wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img' to get the ''cloud'' image."
-	exit 1
+
+if [ ! -e "../../isos/$IMG" ]; then
+    echo "Img not exist."
+    echo "run $ 'wget https://cloud-images.ubuntu.com/focal/current/focal-server-cloudimg-amd64.img' to get the \"cloud\" image, and move it into the isos directory."
+    exit 1
 fi
 
 if [ ! -e "$SEED" ]; then
 	echo "seed does not exist. Run genseed.sh"
 fi
 
+echo "set the image name which qemu will write into(please add filename extemsion like *.img), notice it will overwrite the file at that directory"
+read TMP_TARGET_IMG
+if [[ $TMP_TARGET_IMG != "" ]]; then
+    TARGET_IMG=$TMP_TARGET_IMG
+fi
+
+cp "../../isos/$IMG" "../../isos/$TARGET_IMG"
+
 set -x
 qemu-system-x86_64 \
-  -drive "file=$IMG,format=qcow2" \
+  -drive "file=../../isos/$TARGET_IMG,format=qcow2" \
   -drive "file=$SEED,format=raw" \
   -device rtl8139,netdev=net0 \
   -enable-kvm \
