cefef7f54d81b159ee40c196e839ae051719b909
jerry <efficacy38@gmail.com>
2021-10-21 12:47:33 +0900

    add some comments about transform shell

--- kvm_utils/transform.sh
+++ kvm_utils/transform.sh
@@ -1,4 +1,7 @@
+# convert qcow2 to vmdk
 qemu-img convert -f qcow2 myImage.qcow2 -O vmdk myNewImage.vmdk 
+
+# transform vmdk to fit the current size
 vmkfstools -i myImage.vmdk outputName.vmdk -d thin
 
 # refer from https://stackoverflow.com/questions/37794846/convert-qcow2-to-vmdk-and-make-it-esxi-6-0-compatible
